cfcf81bfbc7147f231424b032cee0fe6a5287d27
TA1137 <muhammad.reza.ft21@mail.umy.ac.id>
2022-10-27 17:18:02 +0700

    Add Method main

--- Search Algorithm/Program.cs
+++ Search Algorithm/Program.cs
@@ -108,6 +108,51 @@ namespace Search_Algorithm
         }
         static void Main(string[] args)
         {
+            Program  myList = new Program();
+            int pilihanmenu;
+            char ch;
+            do
+            {
+
+                do
+                {
+                    Console.WriteLine("Menu Option");
+                    Console.WriteLine("=================");
+                    Console.WriteLine("1. Linear Search");
+                    Console.WriteLine("2. Binary Search");
+                    Console.WriteLine("3. Exit");
+                    Console.WriteLine("Enter your choice (1,2,3) : ");
+                    pilihanmenu = Convert.ToInt32(Console.ReadLine());
+                    switch (pilihanmenu)
+                    {
+                        case 1:
+                            Console.WriteLine("");
+                            Console.WriteLine("................");
+                            Console.WriteLine(" Linear Search ");
+                            Console.WriteLine("................");
+                            myList.input();
+                            myList.LinearSearch();
+                            break;
+                        case 2:
+                            Console.WriteLine("");
+                            Console.WriteLine("................");
+                            Console.WriteLine(" Binary Search ");
+                            Console.WriteLine("................");
+                            myList.input();
+                            myList.BinarySearch();
+                            break;
+                        case 3:
+                            Console.WriteLine("Exit");
+                            break;
+                    }
+                    Console.WriteLine("\nPilih menu lagi? (y/n): ");
+                    ch = char.Parse(Console.ReadLine().ToLower());
+                    Console.Clear();
+                } while (ch == 'y');
+
+                Console.WriteLine("\n\nPress return to exit.");
+                Console.ReadLine();
+            }while(pilihanmenu != 3);
         }
     }
 }
