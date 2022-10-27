c76d5ad997a94e3d14434abca9785d04e035d5c7
TA1137 <muhammad.reza.ft21@mail.umy.ac.id>
2022-10-27 16:24:59 +0700

    Add method input

--- Search Algorithm/Program.cs
+++ Search Algorithm/Program.cs
@@ -8,6 +8,39 @@ namespace Search_Algorithm
 {
     internal class Program
     {
+
+        int[] arr = new int[20];
+
+        int n;
+
+        int i;
+
+
+        public void input()
+        {
+            while(true)
+            {
+                Console.Write("enter the number of element in the array");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if ((n > 0) && (n < 20))
+                    break;
+                else
+                    Console.WriteLine("\nArray should have minimum 1 and maximum 20 element.\n");
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("----------------------");
+            Console.WriteLine(" Enter Array Element ");
+            Console.WriteLine("---------------------");
+            for (i= 0; i < n; i++)
+            {
+                Console.WriteLine("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                arr[i] = Int32.Parse(s1);
+            }
+        }
+
         static void Main(string[] args)
         {
         }
